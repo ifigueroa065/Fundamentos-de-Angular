f4cf0ff1a6fe0b758ce40b55dfeb62d52f2e5342
ifigueroa065 <3059977500302@ingenieria.usac.edu.gt>
2022-11-07 10:38:35 -0600

    Event Binding

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -16,4 +16,23 @@ export class AppComponent {
     age: 21,
     avatar:'https://source.unsplash.com/random'
   }
+
+  toggleButton(){
+    this.btnDisabled=!this.btnDisabled;
+  }
+
+  increaseAge(){
+    this.person.age+=1;
+  }
+
+  onScroll(event : Event){
+    const element=event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+
+  changeName(event : Event){
+    const element=event.target as HTMLInputElement;
+    this.person.name=element.value;
+    console.log(element.value)
+  }
 }
